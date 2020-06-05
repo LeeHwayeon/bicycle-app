baa3c144a37ba45508b6818a4c3229384764e36a
LeeHwayeon <snw4002@hanmail.net>
2020-06-05 18:39:29 +0900

    Update bicycle road switch

--- bicycleApp/app/src/main/java/com/example/bicycleapp/MainActivity.java
+++ bicycleApp/app/src/main/java/com/example/bicycleapp/MainActivity.java
@@ -1,32 +1,64 @@
 package com.example.bicycleapp;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
+import androidx.annotation.NonNull;
+import androidx.annotation.UiThread;
+import androidx.fragment.app.FragmentActivity;
 
 import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    NaverMap map;
+    Switch roadSwitch;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        //지도 불러오는 함수
-        FragmentManager fm = getSupportFragmentManager();
-        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        roadSwitch = (Switch) findViewById(R.id.roadSwitch);
+        roadSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true){
+                    Toast.makeText(MainActivity.this, "자전거도로 표시", Toast.LENGTH_SHORT).show();
+                    //자전거 도로 표시 on
+                    map.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BICYCLE, true);
+
+                }
+                else{
+                    //자전거 도로 표시 off
+                    map.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BICYCLE, false);
+
+                }
+            }
+        });
+
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         if (mapFragment == null) {
             mapFragment = MapFragment.newInstance();
-            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.map, mapFragment).commit();
         }
 
-        mapFragment.getMapAsync((OnMapReadyCallback) this);
+        mapFragment.getMapAsync(this);
+
 
     }
 
+    @UiThread
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        map = naverMap;
+    }
 }
