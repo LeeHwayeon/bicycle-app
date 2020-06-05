e5488db0af73745ebfe237a66585f64de58986ff
LeeHwayeon <snw4002@hanmail.net>
2020-06-05 17:27:50 +0900

    Connect NaverMap

--- bicycleApp/app/src/main/java/com/example/bicycleapp/MainActivity.java
+++ bicycleApp/app/src/main/java/com/example/bicycleapp/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.bicycleapp;
 
 import android.os.Bundle;
-import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
-import net.daum.mf.map.api.MapView;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.OnMapReadyCallback;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,18 +14,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MapView mapView = new MapView(this);
-        RelativeLayout container = (RelativeLayout) findViewById(R.id.map_view);
-        container.addView(mapView);
+        //지도 불러오는 함수
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
 
-
+        mapFragment.getMapAsync((OnMapReadyCallback) this);
 
     }
-
-
 
 }
