105f38a822171e910706696c8951ef7cb10f8794
LeeHwayeon <snw4002@hanmail.net>
2020-06-19 19:34:38 +0900

    hwayeon+jiye

---
+++ merge/BicycleApp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,223 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
+
+import java.util.Calendar;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private static final int ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 100;
+    private FusedLocationSource locationSource;
+
+    private NaverMap naverMap;
+    Switch roadSwitch;
+    private Button btnStart, btnEnd;
+    private TextView tv_id, tv_pass, tv_gender, tv_age, tv_gu;
+
+    Context context;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_id = findViewById(R.id.tv_id);
+        tv_pass = findViewById(R.id.tv_pass);
+        tv_gender = findViewById(R.id.tv_gender);
+        tv_age = findViewById(R.id.tv_age);
+        tv_gu = findViewById(R.id.tv_gu);
+
+        Intent intent = getIntent();
+        String userID = intent.getStringExtra("userID");
+        String userPassword = intent.getStringExtra("userPassword");
+        String userGender = intent.getStringExtra("userGender");
+        String userAge = intent.getStringExtra("userAge");
+        String userGu = intent.getStringExtra("userGu");
+
+        tv_id.setText(userID);
+        tv_pass.setText(userPassword);
+        tv_gender.setText(userGender);
+        tv_age.setText(userAge);
+        tv_gu.setText(userGu);
+
+
+        //특정시간에 사고주의알림
+        new AlarmHATT(getApplicationContext()).Alarm();
+
+        TextView textView = (TextView)findViewById(R.id.textView);
+        String resultText = "값이없음";
+
+        try {
+            resultText = new AccidentParser().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        textView.setText(resultText);
+
+
+        //자전거 도로
+        roadSwitch = (Switch) findViewById(R.id.roadSwitch);
+        roadSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true){
+                    Toast.makeText(MainActivity.this, "자전거도로 표시", Toast.LENGTH_SHORT).show();
+                    //자전거 도로 표시 on
+                    naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BICYCLE, true);
+
+                }
+                else{
+                    //자전거 도로 표시 off
+                    naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BICYCLE, false);
+
+                }
+            }
+        });
+
+        //자전거 사고다발지역표시
+        //setUpAccidentMap();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        //지도 view
+        MapFragment mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        //툴바
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setBackgroundColor(Color.BLACK);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.search:
+                return true;
+            case R.id.favorite:
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull final NaverMap naverMap) {
+        this.naverMap = naverMap;
+
+        //사용자에게 먼저 위치 정보를 허용할지 물어봄
+        locationSource = new FusedLocationSource(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE);
+        naverMap.setLocationSource(locationSource);
+        UiSettings uiSettings = naverMap.getUiSettings();
+        //현위치 ui
+        uiSettings.setLocationButtonEnabled(true);
+
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.5670135, 126.9783740));
+        marker.setMap(naverMap);
+
+
+    }
+
+    public void onRequestPermissionResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case ACCESS_LOCATION_PERMISSION_REQUEST_CODE:
+                locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                return;
+        }
+    }
+
+    /*
+        //자전거 사고 정보
+        public void setUpAccidentMap() {
+            parser parser = new parser();
+            ArrayList<AccidentDTO> accidentDTO = new ArrayList<AccidentDTO>();
+            try {
+                accidentDTO = parser.apiParserSearch();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            for(int i=0; i<accidentDTO.size(); i++){
+                for(AccidentDTO entity : accidentDTO) {
+                    Marker ac_marker = new Marker();
+
+                    ac_marker.setPosition(new LatLng(entity.getLongitude(), entity.getLatitude()));
+                    ac_marker.setCaptionText(entity.getSpot_nm());
+
+                    ac_marker.setMap(naverMap);
+                }
+            }
+        }
+    */
+    //알림
+    public class AlarmHATT {
+        private Context context;
+
+        public AlarmHATT(Context context) {
+            this.context = context;
+        }
+
+        public void Alarm() {
+            AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            Intent intent = new Intent(MainActivity.this, BroadcastD.class);
+
+            PendingIntent sender = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+
+            Calendar calendar = Calendar.getInstance();
+
+            //사고많은 6시 퇴근시간에 알림창 띄우기
+            calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE), 18, 0, 0);
+
+            //알람 예약
+            am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), sender);
+        }
+    }
+
+
+
+}
---
+++ merge/BicycleApp/app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -0,0 +1,136 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private RadioGroup rg_gender;
+    private RadioButton rb_man, rb_woman;
+    private Button btn_register;
+    private String gender;
+    private Spinner spinner1;
+    private Spinner spinner2;
+    private String age;
+    private String gu;
+    private EditText et_id, et_pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        // 아이디 값 찾아주기
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_pass);
+        rg_gender = findViewById(R.id.rg_gender);
+        rb_man = findViewById(R.id.rb_man);
+        rb_woman = findViewById(R.id.rb_woman);
+        btn_register = findViewById(R.id.btn_register);
+        spinner1 = (Spinner)findViewById(R.id.spinner1);
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+        final TextView tv_age = (TextView)findViewById(R.id.tv_age);
+        final TextView tv_gu = (TextView)findViewById(R.id.tv_gu);
+
+        ArrayAdapter spinnerAdapter1;
+        spinnerAdapter1 = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.age));
+        spinner1.setAdapter(spinnerAdapter1);
+        ArrayAdapter spinnerAdapter2;
+        spinnerAdapter2 = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, getResources().getStringArray(R.array.gu));
+        spinner2.setAdapter(spinnerAdapter2);
+
+        rg_gender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                if(i == R.id.rb_man) {
+                    Toast.makeText(RegisterActivity.this, "남성", Toast.LENGTH_SHORT).show();
+                    gender = rb_man.getText().toString();
+                } else if(i == R.id.rb_woman) {
+                    Toast.makeText(RegisterActivity.this, "여성", Toast.LENGTH_SHORT).show();
+                    gender = rb_woman.getText().toString();
+                }
+            }
+        });
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                age = spinner1.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                gu = spinner2.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        // 회원가입 버튼 클릭 시 수행행
+       btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 현재 입력되어 있는 값을 가져온다.
+                String userID = et_id.getText().toString();
+                String userPassword = et_pass.getText().toString();
+                String userGender = gender;
+                String userAge = age;
+                String userGu = gu;
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {  // 회원가입 성공
+                                Toast.makeText(getApplicationContext(), "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            } else {  // 회원가입 실패
+                                Toast.makeText(getApplicationContext(), "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                // 서버로 Volley를 이용해서 요청하기
+                RegisterRequest registerRequest = new RegisterRequest(userID,userPassword,userGender,userAge,userGu, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+
+            }
+        });
+    }
+}
